f570415e728139a76e6bbc79b18ab29533c0061c
YetsinChristian <gcristian755@gmail.com>
2021-10-27 22:00:16 -0500

    prueba laravel

---
+++ php_laravel/laravel8/blog/resources/views/cursos/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.plantilla')
+@section('title','Crear curso')
+@section('content')
+<h1>Aqui puede crear un curso</h1>
+@endsection
---
+++ php_laravel/laravel8/blog/resources/views/cursos/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.plantilla')
+@section('title','Cursos')
+@section('content')
+<h1>Lista de Cursos</h1>
+@endsection
